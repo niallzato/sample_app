8edda93aaa7b0598012ab67e7e840c3e8d0baa91
Niall Brennan <niallbrennan87@gmail.com>
2018-02-25 19:42:08 +0000

    layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title="")
+    base_title="Ruby on Rails tutorial Sample"
+    if page_title==""
+      page_title = base_title
+    else
+      page_title = page_title + " | " + base_title
+    end
+    page_title
+  end
 end
